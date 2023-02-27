63a2f5952ac620686dd585c3bdd595d79701b174
xebec19 <rohant946@gmail.com>
2023-02-28 00:08:52 +0530

    feat: added order module

---
+++ db/query/order.sql
@@ -0,0 +1,9 @@
+-- name: CreateOrder :exec
+CALL create_order($1);
+
+-- name: ListOrder :many
+select order_id, price, delivery_price, total from orders o where user_id = $1;
+
+-- name: GetOrderDetails :many
+select od_id, order_id, product_id, product_price, quantity, delivery_price
+from order_details od where order_id = $1;
---
+++ order/route.go
@@ -0,0 +1,14 @@
+package order
+
+import "github.com/gofiber/fiber"
+
+func SetRoute(app *fiber.App) {
+	router := app.Group("/order")
+	// todo make it a post route
+	// todo and check if user is making order
+	// todo of only those products of which she
+	// todo has made order
+	router.Get("/create-order")
+	router.Get("/read-order")
+	router.Get("/order-details")
+}
